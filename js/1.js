4748330746bc10e26a72c13cf7477fb486393223
alanzzzzz <w604503352@163.com>
2018-06-07 21:10:53 +0800

    change

---
+++ js/1.js
@@ -0,0 +1,42 @@
+$(function(){
+
+/*
+
+    var timer;
+    var n=1;
+/!*
+    var num=60;
+    var num1=1;
+*!/
+
+
+/!*   setTimeout(function(){
+
+       num++;
+       console.log(num)
+   },1000);
+    setInterval(function(){
+        num1++;
+        console.log(num1)
+    },1000);*!/
+    $('#a').click(function(){
+        time=setInterval(function(){
+
+            n++;
+            console.log(n)
+        },1000)
+
+    });
+    $('#b').click(function(){
+        clearInterval(time);
+    })
+
+
+
+
+
+*/
+
+
+
+});
